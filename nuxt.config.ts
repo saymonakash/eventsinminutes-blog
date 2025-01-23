5603e1d9a325ab86733e4d779a77c907a8ae6385
Saymon Arefin Akash <saymon.akash@lifted.dev>
2025-01-23 21:02:13 +0600

    supabase key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -146,6 +146,8 @@ export default defineNuxtConfig({
   // Supabase Configuration
   supabase: {
     redirect: false,
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
   },
 
   sitemap: {
